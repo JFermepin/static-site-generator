1273b6531870f064efef86dedd5310dc1c3a5678
JFermepin <jfermepingaldo@frba.utn.edu.ar>
2025-05-31 19:01:53 +0000

    Creada carpeta docs para alojar en la web de github

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
+import sys
 from textnode import TextNode, TextType
 from copystatic import copy_files_recursive
 from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 def main():
+
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    
     copy_files_recursive(dir_path_static, dir_path_public)
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 main()
 
